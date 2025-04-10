a84546fa89be5e73387a1afb92e6ae10e8aa6aeb
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:14:58 +0200

    Update test_bgeu.s

--- test_cases/test_cases/test_bgeu.s
+++ test_cases/test_cases/test_bgeu.s
@@ -1,8 +1,7 @@
 # test_bgeu.s
-0xFFF00093 #addi x1, x0, -1      # x1 = 0xFFFFFFFF
-addi x2, x0, 1       # x2 = 1
-0x00100113 #bgeu x1, x2, label
-0x00000513 #addi x10, x0, 0
-label:
-0x00100513 #addi x10, x0, 1      # expected: x10 = 1
+0xFFF00093   # addi x1, x0, -1
+0x00100113   # addi x2, x0, 1
+0x00857163   # bgeu x1, x2, 8
+0x00000513   # addi x10, x0, 0
+0x00100513   # addi x10, x0, 1
 
