0f7f81c9c1169bc83df91988a19de70be687e63b
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 19:49:53 +0200

    Update test_sb.s

--- test_cases/test_cases/test_sb.s
+++ test_cases/test_cases/test_sb.s
@@ -1,5 +1,6 @@
 # test_sb.s
-addi x1, x0, 97       # x1 = 97 ('a' in ASCII)
-lui  x2, 0x00010      # x2 = 0x10000
-sb   x1, 0(x2)        # store byte to Mem[0x10000]
+0x06100093   # addi x1, x0, 97
+0x00010537   # lui x2, 0x00010
+0x00110023   # sb x1, 0(x2)
+
 
