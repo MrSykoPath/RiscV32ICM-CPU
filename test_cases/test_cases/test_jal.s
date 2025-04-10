619e6cbe7702624dcd2e2e1b13d8793fdf9d300a
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:17:56 +0200

    Update test_jal.s

--- test_cases/test_cases/test_jal.s
+++ test_cases/test_cases/test_jal.s
@@ -1,6 +1,6 @@
 # test_jal.s
-0x008000EF  #jal x1, label         # x1 = PC + 4 # jal x1, label (8-byte forward jump)
-0x00000513 #addi x10, x0, 0       # skipped
-label:
-0x00100513 #addi x10, x0, 1       # x10 = 1, reached via jump
+0x008000EF   # jal x1, 8
+0x00000513   # addi x10, x0, 0
+0x00100513   # addi x10, x0, 1
+
 
