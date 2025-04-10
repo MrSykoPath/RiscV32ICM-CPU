2d261caf73f4c0d059dad6b0ca3717bf2d661de6
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 19:18:56 +0200

    Update test_beq.s

--- test_cases/test_cases/test_beq.s
+++ test_cases/test_cases/test_beq.s
@@ -1,8 +1,8 @@
 # test_beq.s
-addi x1, x0, 5
-addi x2, x0, 5
-beq  x1, x2, label
-addi x10, x0, 0      # won't execute if branch is taken
+0x00500093 #addi x1, x0, 5
+0x00500113 #addi x2, x0, 5
+0x00050663 #beq  x1, x2, label
+0x00000513 #addi x10, x0, 0      # won't execute if branch is taken
 label:
-addi x10, x0, 1      # expected: x10 = 1
+0x00100513 #addi x10, x0, 1      # expected: x10 = 1
 
