04e919df893f267cd149d5f17c9809c87315ac7f
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 19:30:23 +0200

    Update test_blt.s

--- test_cases/test_cases/test_blt.s
+++ test_cases/test_cases/test_blt.s
@@ -1,8 +1,8 @@
 # test_blt.s
-addi x1, x0, 2
-addi x2, x0, 4
-blt  x1, x2, label
-addi x10, x0, 0
+0x00200093 #addi x1, x0, 2
+0x00400113 #addi x2, x0, 4
+0x00054663 #blt  x1, x2, label
+0x00000513 #addi x10, x0, 0
 label:
-addi x10, x0, 1      # expected: x10 = 1
+0x00100513 #addi x10, x0, 1      # expected: x10 = 1
 
