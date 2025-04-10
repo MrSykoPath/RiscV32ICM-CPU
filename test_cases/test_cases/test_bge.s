be5a85aa858cce5c830c442d1a168f2513137c82
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:14:03 +0200

    Update test_bge.s

--- test_cases/test_cases/test_bge.s
+++ test_cases/test_cases/test_bge.s
@@ -1,8 +1,6 @@
 # test_bge.s
-0x00500093 #addi x1, x0, 5
-0x00400113 #addi x2, x0, 4
-0x00050663 #bge  x1, x2, label
-0x00000513 #addi x10, x0, 0
-label:
-0x00100513 #addi x10, x0, 1      # expected: x10 = 1
-
+0x00500093   # addi x1, x0, 5
+0x00400113   # addi x2, x0, 4
+0x00855663   # bge x1, x2, 8
+0x00000513   # addi x10, x0, 0
+0x00100513   # addi x10, x0, 1
