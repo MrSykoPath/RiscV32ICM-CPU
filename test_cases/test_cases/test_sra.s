7e98f42e3caedfeaadbf231269d821f9e01db70f
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:03:33 +0200

    Update test_sra.s

--- test_cases/test_cases/test_sra.s
+++ test_cases/test_cases/test_sra.s
@@ -1,5 +1,6 @@
 # test_sra.s
-addi x1, x0, -8
-addi x2, x0, 1
-sra  x3, x1, x2      # x3 = -4 (preserves sign bit)
+0xFF800093   # addi x1, x0, -8
+0x00100113   # addi x2, x0, 1
+0x4020D1B3   # sra x3, x1, x2
+
 
