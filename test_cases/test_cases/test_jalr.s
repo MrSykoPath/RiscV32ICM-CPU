39ed79e937c9cefcd95939c6104154d7342fe0ee
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:18:46 +0200

    Update test_jalr.s

--- test_cases/test_cases/test_jalr.s
+++ test_cases/test_cases/test_jalr.s
@@ -1,7 +1,5 @@
 # test_jalr.s
-0x00C00093 #addi x1, x0, 12       # x1 = target offset
-0x00008167 #jalr x2, x1, 0        # x2 = return address, jump to PC = x1
-0x00000513   # addi x10, x0, 0 (skipped if jump works)
-# jump target should point to:
+0x00C00093   # addi x1, x0, 12
+0x00008167   # jalr x2, x1, 0
+0x00000513   # addi x10, x0, 0 (skipped)
 0x00100513   # addi x10, x0, 1 (target)
-
