20a338e6f7ef9b736a37fb39bef8c8103781849d
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 19:34:52 +0200

    Update test_bne.s

--- test_cases/test_cases/test_bne.s
+++ test_cases/test_cases/test_bne.s
@@ -1,8 +1,8 @@
 # test_bne.s
-addi x1, x0, 5
-addi x2, x0, 3
-bne  x1, x2, label
-addi x10, x0, 0
+0x00500093 #addi x1, x0, 5
+0x00300113 #addi x2, x0, 3
+0x00051663 #bne  x1, x2, label
+0x00000513 #addi x10, x0, 0
 label:
-addi x10, x0, 1      # expected: x10 = 1
+0x00100513 #addi x10, x0, 1      # expected: x10 = 1
 
