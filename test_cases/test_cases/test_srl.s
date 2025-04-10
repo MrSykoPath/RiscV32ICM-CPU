06709afff41ca2c5ad4cdb4b3461f050d3d2388d
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:04:54 +0200

    Update test_srl.s

--- test_cases/test_cases/test_srl.s
+++ test_cases/test_cases/test_srl.s
@@ -1,5 +1,4 @@
 # test_srl.s
-addi x1, x0, 16
-addi x2, x0, 2
-srl  x3, x1, x2      # x3 = 16 >> 2 = 4
-
+0x01000093   # addi x1, x0, 16
+0x00200113   # addi x2, x0, 2
+0x0020D1B3   # srl x3, x1, x2
