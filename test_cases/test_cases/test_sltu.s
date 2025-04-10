8214e0457a97d4442b2ca0bf8da348d26ee525cd
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 20:01:50 +0200

    Update test_sltu.s

--- test_cases/test_cases/test_sltu.s
+++ test_cases/test_cases/test_sltu.s
@@ -1,5 +1,4 @@
 # test_sltu.s
-lui  x1, 0x80000     # x1 = large positive number (2^31)
-addi x2, x0, 1
-sltu x3, x2, x1      # x3 = 1 (1 < 2^31)
-
+0x80000537   # lui x1, 0x80000
+0x00100113   # addi x2, x0, 1
+0x0011B1B3   # sltu x3, x2, x1
