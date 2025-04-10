6eddc5583da968625eeae2e294b226ed40a67be5
nadineelgarem <98808670+nadineelgarem@users.noreply.github.com>
2025-04-10 19:32:53 +0200

    Update test_bltu.s

--- test_cases/test_cases/test_bltu.s
+++ test_cases/test_cases/test_bltu.s
@@ -1,8 +1,8 @@
 # test_bltu.s
-lui x1, 0x00001      # x1 = 0x1000
-addi x2, x0, -1      # x2 = 0xFFFFFFFF (unsigned > x1)
-bltu x1, x2, label
-addi x10, x0, 0
+0x000010B7 #lui x1, 0x00001      # x1 = 0x1000
+0xFFF00113 #addi x2, x0, -1      # x2 = 0xFFFFFFFF (unsigned > x1)
+0x0005C663 #bltu x1, x2, label
+0x00000513 #addi x10, x0, 0
 label:
-addi x10, x0, 1      # expected: x10 = 1
+0x00100513 #addi x10, x0, 1      # expected: x10 = 1
 
